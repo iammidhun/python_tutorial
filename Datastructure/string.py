98eca86951c73cf052d58f4b463c8ad3c7000331
iammidhun <midhunmohanputhur@gmail.com>
2019-09-15 12:41:47 +0530

    string concept in python added

---
+++ Datastructure/string.py
@@ -0,0 +1,26 @@
+strings are set of characters enclosed by single or double quotes
+"Hii" or 'Hii'
+
+a = 'Hii'
+print(a)
+
+a= "Hii"
+print(a)
+
+MUltiple string
+a = """
+Created by Guido van Rossum and first released in 1991, 27 years old.
+Beautiful is better than ugly
+Explicit is better than implicit
+Simple is better than complex which is better than complicated
+Readability counts
+Extensible to very high level
+"""
+
+is string is an Array ?
+yes 
+a = "hiii midhun"
+
+print(a[0])  = 'h'
+print(a[1])  = 'i'
+print(a[-1]) = 'n'
