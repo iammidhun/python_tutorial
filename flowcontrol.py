be3f8695bbae588dbab25a970cb5fb7f287df295
iammidhun <midhunmohanputhur@gmail.com>
2019-12-02 08:13:51 +0530

    boolean added in flow control

---
+++ flowcontrol.py
@@ -0,0 +1,13 @@
+
+    Equals: a == b
+    Not Equals: a != b
+    Less than: a < b
+    Less than or equal to: a <= b
+    Greater than: a > b
+    Greater than or equal to: a >= b
+
+#an if statement is written by using if keyword
+
+name  =  "midhun"
+if (name=='midhun'):
+	print ("okay his name is correct")
