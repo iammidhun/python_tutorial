8c4951250168e6e386a4679c7a55d7d78b25c56f
iammidhun <midhunmohanputhur@gmail.com>
2019-12-04 07:41:01 +0530

    example of try-except with Attribute error

--- error_handling.py
+++ error_handling.py
@@ -12,3 +12,16 @@ except:
 
 "here the error will execute if it is not defined in the try block"
 #strip is the property of srting 
+
+#excepting more exception
+
+no = 25
+try:
+	no.strip()
+except AttributeError:
+	print("it is an AttributeError")
+except:
+	print("sorry")
+
+#'int' object has no attribute 'strip' it is an attribute error so except Attributeerror will occur first
+the output will be "it is an AttributeError"
