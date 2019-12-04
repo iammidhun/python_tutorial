4da2024966c69676e64d7461988d4fc7ba3b8828
iammidhun <midhunmohanputhur@gmail.com>
2019-12-04 07:35:26 +0530

    example of try-except

--- error_handling.py
+++ error_handling.py
@@ -4,3 +4,11 @@
 "finally lets you to execute the code"
 
 #example
+no = 25
+try:
+	no.strip()
+except:
+	print("sorry it is not a no")
+
+"here the error will execute if it is not defined in the try block"
+#strip is the property of srting 
