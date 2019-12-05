e11a385cb7888457f26ac1166eb4f9fd4955eeef
iammidhun <midhunmohanputhur@gmail.com>
2019-12-05 08:36:09 +0530

    the basic example added

--- functions/functions.py
+++ functions/functions.py
@@ -4,3 +4,12 @@
 def function_name ():
 	pass
 "this is the basic structure of a function "
+"def is followed by the function name and parentheses ()"
+"Argument should be placed inside the parentheses"
+
+def name(name):
+	print(name)
+
+name("midhun") #function call
+#the output will be 
+midhun
