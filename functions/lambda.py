406aded8efae0899bc120aa694ecfbbe3ea1d82c
iammidhun <midhunmohanputhur@gmail.com>
2019-12-06 14:14:11 +0530

    lambda function example added

--- functions/lambda.py
+++ functions/lambda.py
@@ -1,2 +1,11 @@
 "A lambda function is a small anonymous function"
 "An anonymous function is a function that has no name"
+"It will takes any number of argument , but can only have one expression"
+
+#example for lambda function
+x =  lambda x : x*5
+
+print(x(4))
+
+output : 20
+
