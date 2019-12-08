20f808b54c65bf19e2036ab4b87ad968f115a8ce
iammidhun <midhunmohanputhur@gmail.com>
2019-12-08 11:13:13 +0530

    class concept added

--- oops/oops.py
+++ oops/oops.py
@@ -7,3 +7,17 @@ object has attribute and behaviour
 
 for example student is an object , it has attribute "name", "age"
 the functions that he does is his behaviour na?
+
+class 
+"class is a group of object"
+
+syntax 
+
+class classname():
+	def __init__():
+	......................................
+	...............................
+	......................................
+
+
+	 
