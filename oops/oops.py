50d5d1a6cec1194eab83853ee49df18ca2e29ca1
iammidhun <midhunmohanputhur@gmail.com>
2019-12-08 11:09:28 +0530

    attribute and behaviour concept added

--- oops/oops.py
+++ oops/oops.py
@@ -2,3 +2,8 @@
 object
 object is an real word entity 
 for example student , office , pen etc..
+
+object has attribute and behaviour
+
+for example student is an object , it has attribute "name", "age"
+the functions that he does is his behaviour na?
